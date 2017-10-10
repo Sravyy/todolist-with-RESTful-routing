16dde90e126d2abc31fb36e697ff4fc5b8ac3a8d
Saneyee Sarkar and Sravanthi Velaga <sravanthi@>
2017-10-10 15:12:19 -0700

    Todo list with RESTful Routing

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+
+namespace ToDoList.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/tasks/new")]
+        public ActionResult TaskForm()
+
+        {
+            return View();
+        }
+
+        [HttpPost("/tasks")]
+        public ActionResult AddTask()
+        {
+            Task newTask = new Task(Request.Form["new-task"]);
+            List<Task> allTasks = Task.GetAll();
+            return View("Tasks", allTasks);
+        }
+
+        [HttpGet("/tasks/{id}")]
+        public ActionResult TaskDetail(int id)
+        {
+            Task task = Task.Find(id);
+            return View(task);
+        }
+
+        [HttpPost("/task/list/clear")]
+         public ActionResult TaskListClear()
+         {
+             Task.ClearAll();
+             return View();
+         }
+
+  }
+}
